25732f9c140babf7d690d3402713572a7b97324f
sk <content.type.text.net@gmail.com>
2024-03-15 21:57:20 +0900

    やったはいいけど これ .net バージョンないと不整合起きんかなぁ、、、最新版しか見ないしまぁいっかぁ・・・

--- src/VsLocalizedIntellisense.Raw/VsLocalizedIntellisense.Raw/Models/Host/ApplicationHostedService.cs
+++ src/VsLocalizedIntellisense.Raw/VsLocalizedIntellisense.Raw/Models/Host/ApplicationHostedService.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -28,9 +30,32 @@ namespace VsLocalizedIntellisense.Raw.Models.Host
 
         #endregion
 
+        #region function
+
+        private async Task DownloadLibraryAsync(Uri uri, string installDirectoryPath, HttpClient httpClient)
+        {
+            using var stream = await httpClient.GetStreamAsync(uri);
+            using var archive = new ZipArchive(stream);
+
+            var refItems = archive.Entries
+                // くっそ適当
+                .Where(a => a.FullName.Contains("ref") && Path.GetExtension(a.Name) == ".xml")
+                .ToArray()
+            ;
+
+            foreach(var refItem in refItems) {
+                var installFilePath = Path.Combine(installDirectoryPath, refItem.Name);
+                using var entryStream = refItem.Open();
+                using var saveStream = new FileStream(installFilePath, FileMode.Create);
+                await entryStream.CopyToAsync(saveStream);
+            }
+        }
+
+        #endregion
+
         #region IHostedService
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
             Logger.LogInformation("hello world!");
 
@@ -38,8 +63,6 @@ namespace VsLocalizedIntellisense.Raw.Models.Host
             if(string.IsNullOrWhiteSpace(installBaseDirectoryPath)) {
                 throw new InvalidOperationException("install_base_dir");
             }
-
-            var httpClient = HttpClientFactory.CreateClient();
 
             var nugetBaseUrl = Configuration.GetValue<string>("nuget_base_url");
             if(string.IsNullOrWhiteSpace(nugetBaseUrl)) {
@@ -49,14 +72,27 @@ namespace VsLocalizedIntellisense.Raw.Models.Host
             if(string.IsNullOrWhiteSpace(nugetDownloadPath)) {
                 throw new InvalidOperationException("nuget_download_path");
             }
+
             var librariesSection = Configuration.GetSection("libraries");
             var libraries = librariesSection.Get<string[]>() ?? throw new ApplicationException("libraries");
-            foreach(var library in libraries) {
-                var uri = UrlHelper.JoinUri(nugetBaseUrl, nugetDownloadPath, library);
-                Logger.LogInformation("{library}: {uri}", library, uri);
+            var libraryMapping = Configuration.GetSection("mapping").GetChildren()
+                  .ToDictionary(k => k.Key, v => v.Value)
+            ;
+            if(libraries.Length != libraryMapping.Count) {
+                throw new ApplicationException("libraries.Length != libraryMapping.Count");
             }
 
-            return Task.CompletedTask;
+            var httpClient = HttpClientFactory.CreateClient();
+
+            foreach(var library in libraries) {
+                var installDirectoryPath = Path.Combine(installBaseDirectoryPath, libraryMapping[library] ?? throw new ArgumentException(library));
+                if(!Directory.Exists(installDirectoryPath)) {
+                    Directory.CreateDirectory(installDirectoryPath);
+                }
+                var uri = UrlHelper.JoinUri(nugetBaseUrl, nugetDownloadPath, library);
+                Logger.LogInformation("{library}: {installDirectoryPath} {uri}", library, installDirectoryPath, uri);
+                await DownloadLibraryAsync(uri, installDirectoryPath, httpClient);
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
