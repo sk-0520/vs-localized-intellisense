8de15f5b0e09f695be694b4af7d67ad2c196ce43
sk <content.type.text.net@gmail.com>
2024-03-31 19:45:22 +0900

    startup wpf

--- src/VsLocalizedIntellisense.Diff/VsLocalizedIntellisense.Diff/App.xaml.cs
+++ src/VsLocalizedIntellisense.Diff/VsLocalizedIntellisense.Diff/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using VsLocalizedIntellisense.Diff.Models.Element;
+using VsLocalizedIntellisense.Diff.ViewModels;
+using VsLocalizedIntellisense.Diff.Views;
 
 namespace VsLocalizedIntellisense.Diff
 {
@@ -9,6 +12,22 @@ namespace VsLocalizedIntellisense.Diff
     /// </summary>
     public partial class App: Application
     {
-    }
+        #region Application
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var model = new MainElement();
+            var viewModel = new MainViewModel(model);
+
+            var view = new MainWindow() {
+                DataContext = viewModel,
+            };
+            MainWindow = view;
+            MainWindow.Show();
+        }
+
+        #endregion
+    }
 }
