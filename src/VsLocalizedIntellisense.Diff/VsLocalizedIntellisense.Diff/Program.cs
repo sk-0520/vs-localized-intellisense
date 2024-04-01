3da9fad216dcbd7545e8ed558fb742aa4b1885fe
sk <content.type.text.net@gmail.com>
2024-04-01 19:10:39 +0900

    まぁこれでいっか

--- src/VsLocalizedIntellisense.Diff/VsLocalizedIntellisense.Diff/Program.cs
+++ src/VsLocalizedIntellisense.Diff/VsLocalizedIntellisense.Diff/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VsLocalizedIntellisense.Diff.Models;
 using VsLocalizedIntellisense.Diff.Models.Element;
 using VsLocalizedIntellisense.Diff.Models.Host;
 using VsLocalizedIntellisense.Diff.ViewModels;
@@ -18,14 +22,22 @@ namespace VsLocalizedIntellisense.Diff
         [STAThread]
         public static void Main(string[] args)
         {
+            //var appPath = new AppPath();
+
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configure => {
+                    var applicationDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    configure.SetBasePath(applicationDirectory!);
+                })
                 .ConfigureServices(services => {
                     services.AddHostedService<MainHostedService>();
+                    services.AddSingleton<IAppPath, AppPath>();
                     services.AddSingleton<Application, App>();
                     services.AddSingleton<MainElement>();
                     services.AddSingleton<MainViewModel>();
                     services.AddTransient<MainWindow>();
                 })
+
                 .Build()
                 .Run()
             ;
